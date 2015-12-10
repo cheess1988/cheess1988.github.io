d63a7e19c1ffa00edbb617575dccfe5cce4a83d2
CheeSS <cheess1988@gmail.com>
2015-12-10 16:17:21 +0200

    move to js file

---
+++ projects/phonebook/scripts/main.js
@@ -0,0 +1,97 @@
+var phoneBook = angular.module('phoneBook', [])
+	.controller('phoneBookCtrl', function($scope) {
+		$scope.currentId = -1;
+		$scope.phonebook = [{
+			name: 'Dmitriy Pristupa',
+			number: '+1234567890',
+			email: 'email@email.com',
+			photo: 'img/photo.jpg'
+		},
+		{
+			name: 'John Doe',
+			number: '+1234567890',
+			email: 'email@email.com',
+			photo: 'img/profile.jpg'
+		},
+		{
+			name: 'John Doe 2',
+			number: '+2222222222',
+			email: 'test@test.com',
+			photo: 'img/profile.jpg'
+		},
+		{
+			name: 'John Doe 3',
+			number: '+3333333333',
+			email: 'test@test.com',
+			photo: 'img/profile.jpg'
+		},
+		{
+			name: 'John Doe 4',
+			number: '+4444444444',
+			email: 'test@test.com',
+			photo: 'img/profile.jpg'
+		},
+		{
+			name: 'John Doe 5',
+			number: '+5555555555',
+			email: 'test@test.com',
+			photo: 'img/profile.jpg'
+		},
+		{
+			name: 'John Doe 6',
+			number: '+6666666666',
+			email: 'test@test.com',
+			photo: 'img/profile.jpg'
+		},
+		{
+			name: 'John Doe 7',
+			number: '+7777777777',
+			email: 'test@test.com',
+			photo: 'img/profile.jpg'
+		},
+		{
+			name: 'John Doe 8',
+			number: '+8888888888',
+			email: 'test@test.com',
+			photo: 'img/profile.jpg'
+		},
+		{
+			name: 'John Doe 9',
+			number: '+9999999999',
+			email: 'test@test.com',
+			photo: 'img/profile.jpg'
+		}];
+		$scope.addContact = function() {
+			if($scope.name !== '') {
+				$scope.phonebook.push({
+					name: $scope.name,
+					number: $scope.number,
+					email: $scope.email,
+					photo: $scope.photo
+				});
+				$scope.name = '';
+				$scope.number = '';
+				$scope.email = '';
+			}
+		};
+		$scope.saveContact = function() {
+			if($scope.currentId > -1) {
+				$scope.phonebook[$scope.currentId].name = $scope.name;
+				$scope.phonebook[$scope.currentId].number = $scope.number;
+				$scope.phonebook[$scope.currentId].email = $scope.email;
+				$scope.name = '';
+				$scope.number = '';
+				$scope.email = '';
+				$scope.currentId = -1;
+			}
+		};
+		$scope.editContact = function(currentId) {
+			$scope.currentId = currentId;
+			$scope.name = $scope.phonebook[currentId].name;
+			$scope.number = $scope.phonebook[currentId].number;
+			$scope.email = $scope.phonebook[currentId].email;
+		};
+		$scope.deleteContact = function(currentId) {
+			$scope.phonebook.splice(currentId, 1);
+		};
+	})
